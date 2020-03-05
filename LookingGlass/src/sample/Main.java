a7e4757d7765bba137af1687f69952ce71817bb6
ulloaza <61514120+ulloaza@users.noreply.github.com>
2020-03-05 14:08:33 -0600

    Add files via upload

---
+++ LookingGlass/src/sample/Main.java
@@ -0,0 +1,70 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+    private static final int shadowSize = 50;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        StackPane stackPane = new StackPane(createShadowPane());
+        stackPane.setStyle(
+                "-fx-background-color: rgba(255, 255, 255, 0.7);" +
+                        "-fx-background-insets: " + shadowSize + ";"
+        );
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Looking Glass v 1.0");
+        Pane maskPane =  FXMLLoader.load(getClass().getResource("sample.fxml"));
+        stackPane.getChildren().add(maskPane);
+        Scene mainScene = new Scene(stackPane, 1250, 750);
+        mainScene.setFill(Color.TRANSPARENT);
+        maskPane.setLayoutX(200);
+        maskPane.setLayoutY(300);
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
+    }
+
+    private Pane createShadowPane() {
+        Pane shadowPane = new Pane();
+        shadowPane.setStyle(
+                "-fx-background-color: white;" +
+                        "-fx-effect: dropshadow(gaussian, darkred, " + shadowSize + ", 0, 0, 0);" +
+                        "-fx-background-insets: " + shadowSize + ";"
+        );
+
+        Rectangle innerRect = new Rectangle();
+        Rectangle outerRect = new Rectangle();
+        shadowPane.layoutBoundsProperty().addListener(
+                (observable, oldBounds, newBounds) -> {
+                    innerRect.relocate(
+                            newBounds.getMinX() + shadowSize,
+                            newBounds.getMinY() + shadowSize
+                    );
+                    innerRect.setWidth(newBounds.getWidth() - shadowSize * 2);
+                    innerRect.setHeight(newBounds.getHeight() - shadowSize * 2);
+
+                    outerRect.setWidth(newBounds.getWidth());
+                    outerRect.setHeight(newBounds.getHeight());
+
+                    Shape clip = Shape.subtract(outerRect, innerRect);
+                    shadowPane.setClip(clip);
+                }
+        );
+
+        return shadowPane;
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
